ad548d2dbf11d623b402c334e390d5be4089df87
PolinaRrr <poniapocalypsisa@gmail.com>
2021-01-19 19:46:17 +0300

    Commit#2 Добавлены обработчики кнопок, написаны методы обработки математических действие и отображения результата В виду катастрофической нехватки времени, бызовый функционал был пересмотрен. В следующей версии будут внесены функциональные кнопки, будет отработана горизонтальная ориентация

--- app/src/main/java/com/example/hw_1_2_05_01_2021/MainActivity.java
+++ app/src/main/java/com/example/hw_1_2_05_01_2021/MainActivity.java
@@ -10,12 +10,84 @@ package com.example.hw_1_2_05_01_2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private CalculatorLogic calculator;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // массив Id  цифровых кнопок
+        int[] numberIds = new int[]{
+                R.id.button_bt_0,
+                R.id.button_bt_1,
+                R.id.button_bt_2,
+                R.id.button_bt_3,
+                R.id.button_bt_4,
+                R.id.button_bt_5,
+                R.id.button_bt_6,
+                R.id.button_bt_7,
+                R.id.button_bt_8,
+                R.id.button_bt_9,
+
+        };
+
+        //массив Id кнопок действий
+        int[] actionIds = new int[]{
+                R.id.button_addition,
+                R.id.button_division,
+                R.id.button_multiplication,
+                R.id.button_subtracting,
+                R.id.button_result,
+
+        };
+
+        text = findViewById(R.id.textView_print_result);
+        calculator = new CalculatorLogic();
+
+        //создаю обработчик события нажатия цифровой кнопки
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.onNumberPressed(v.getId());
+                text.setText(calculator.getText());
+            }
+        };
+
+        // создаю обработчик события нажатия кнопки действия
+        View.OnClickListener actionButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.onActionPressed(v.getId());
+                text.setText(calculator.getText());
+            }
+        };
+
+
+        //создаю обработчик события при нажатии на кнопку сброса
+        findViewById(R.id.button_reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.reset();
+                text.setText(calculator.getText());
+            }
+        });
+
+        //прохожу циклом по массиву и для каждой кнопки устанавливаю обработчик
+        for (int i = 0; i < numberIds.length; i++) {
+            findViewById(numberIds[i]).setOnClickListener(numberButtonClickListener);
+        }
+
+        for (int i = 0; i < actionIds.length; i++) {
+            findViewById(actionIds[i]).setOnClickListener(actionButtonClickListener);
+        }
+
+
+
     }
 }
